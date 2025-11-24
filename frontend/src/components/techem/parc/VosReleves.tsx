31b894611f2f8d117d6c3dad95d30181982df173
VascoII <fabriceds@yahoo.fr>
2025-11-24 10:21:56 +0100

    add: design parc view

--- frontend/src/components/techem/parc/VosReleves.tsx
+++ frontend/src/components/techem/parc/VosReleves.tsx
@@ -104,24 +104,6 @@ export default function VosReleves() {
               Vos Relevés
             </h3>
           </div>
-          <div className="relative inline-block">
-            <button onClick={toggleDropdown} className="dropdown-toggle">
-              <MoreDotIcon className="text-gray-400 hover:text-gray-700 dark:hover:text-gray-300" />
-            </button>
-            <Dropdown
-              isOpen={isOpen}
-              onClose={closeDropdown}
-              className="w-40 p-2"
-            >
-              <DropdownItem
-                tag="a"
-                onItemClick={closeDropdown}
-                className="flex w-full font-normal text-left text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
-              >
-                Voir plus
-              </DropdownItem>
-            </Dropdown>
-          </div>
         </div>
         <div className="relative ">
           <div className="max-h-[330px]">
