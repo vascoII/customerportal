98dafbd0e10ce26b9e4f0c5b6bf2ac96e0467eff
DA SILVA, Fabrice <fabrice.da-silva.extern@techem.fr>
2025-11-27 15:16:28 +0100

    update: 404 page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -28,7 +28,7 @@ export default function NotFound() {
         />
 
         <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
-          We can’t seem to find the page you are looking for!
+          Nous ne parvenons pas à trouver la page que vous recherchez !
         </p>
 
         <Link
@@ -40,7 +40,7 @@ export default function NotFound() {
       </div>
       {/* <!-- Footer --> */}
       <p className="absolute text-sm text-center text-gray-500 -translate-x-1/2 bottom-6 left-1/2 dark:text-gray-400">
-        &copy; {new Date().getFullYear()} - TailAdmin
+        &copy; {new Date().getFullYear()} - Techem
       </p>
     </div>
   );
