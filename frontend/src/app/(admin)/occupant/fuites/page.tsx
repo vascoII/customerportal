2329c547620374f977df7b134a425c701897f4f6
VascoII <fabriceds@yahoo.fr>
2025-12-02 11:34:39 +0100

    update: occupant pages

--- frontend/src/app/(admin)/occupant/fuites/page.tsx
+++ frontend/src/app/(admin)/occupant/fuites/page.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 import { Metadata } from "next";
 import OccupantFuitesPageClient from "./OccupantFuitesPageClient";
 
--- frontend/src/app/(admin)/occupant/interventions/page.tsx
+++ frontend/src/app/(admin)/occupant/interventions/page.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 import { Metadata } from "next";
 import OccupantInterventionsPageClient from "./OccupantInterventionsPageClient";
 
